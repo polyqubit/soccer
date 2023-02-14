426e629ca072117a5f611e80878215f4cd433e96
polyqubit <polyumbrial@gmail.com>
2023-02-14 09:09:16 -0800

    greate game file

---
+++ src/pages/game.js
@@ -0,0 +1,11 @@
+import Head from "next/head"
+
+export default function Game() {
+    return (
+        <>
+            <Head>
+                <title>Game</title>
+            </Head>
+        </>
+    )
+}
