4a06c72209aa4c3f2b70701e3650c65daa453c04
polyqubit <polyumbrial@gmail.com>
2023-03-04 01:08:08 -0800

    demo

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
+import styles from "@styles/Demo.module.css"
 
 export default function Demonstrative() {
     return <>
         <Head>
             <title>Demo</title>
         </Head>
+        <main>
+            <p className={styles.body}>
+                hi
+            </p>
+        </main>
     </>
 }
